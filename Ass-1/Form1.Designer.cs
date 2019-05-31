2bbde3226f08266b755088adb6b5353c4b4fbbdb
Erica Borges Teixeira <ericaborges.t@gmail.com>
2019-05-31 12:30:16 -0400

    First commit

---
+++ Ass-1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Ass_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.txtOctal = new System.Windows.Forms.TextBox();
+            this.txtHexadecimal = new System.Windows.Forms.TextBox();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.txtBinary = new System.Windows.Forms.TextBox();
+            this.lblOctal = new System.Windows.Forms.Label();
+            this.lblHexadecimal = new System.Windows.Forms.Label();
+            this.lblDecimal = new System.Windows.Forms.Label();
+            this.lblBinary = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(388, 376);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(4);
+            this.btnExit.Size = new System.Drawing.Size(162, 56);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(224, 376);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Padding = new System.Windows.Forms.Padding(4);
+            this.btnClear.Size = new System.Drawing.Size(150, 56);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverter.Location = new System.Drawing.Point(63, 376);
+            this.btnConverter.Margin = new System.Windows.Forms.Padding(0);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Padding = new System.Windows.Forms.Padding(4);
+            this.btnConverter.Size = new System.Drawing.Size(150, 56);
+            this.btnConverter.TabIndex = 6;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.BtnConverter_Click);
+            // 
+            // txtOctal
+            // 
+            this.txtOctal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOctal.Location = new System.Drawing.Point(467, 270);
+            this.txtOctal.Margin = new System.Windows.Forms.Padding(0);
+            this.txtOctal.Name = "txtOctal";
+            this.txtOctal.ReadOnly = true;
+            this.txtOctal.Size = new System.Drawing.Size(83, 32);
+            this.txtOctal.TabIndex = 5;
+            // 
+            // txtHexadecimal
+            // 
+            this.txtHexadecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHexadecimal.Location = new System.Drawing.Point(323, 270);
+            this.txtHexadecimal.Margin = new System.Windows.Forms.Padding(0);
+            this.txtHexadecimal.Name = "txtHexadecimal";
+            this.txtHexadecimal.ReadOnly = true;
+            this.txtHexadecimal.Size = new System.Drawing.Size(94, 32);
+            this.txtHexadecimal.TabIndex = 4;
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecimal.Location = new System.Drawing.Point(195, 270);
+            this.txtDecimal.Margin = new System.Windows.Forms.Padding(0);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.ReadOnly = true;
+            this.txtDecimal.Size = new System.Drawing.Size(97, 32);
+            this.txtDecimal.TabIndex = 3;
+            // 
+            // txtBinary
+            // 
+            this.txtBinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBinary.Location = new System.Drawing.Point(77, 270);
+            this.txtBinary.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBinary.Name = "txtBinary";
+            this.txtBinary.ReadOnly = true;
+            this.txtBinary.Size = new System.Drawing.Size(101, 32);
+            this.txtBinary.TabIndex = 2;
+            // 
+            // lblOctal
+            // 
+            this.lblOctal.AutoSize = true;
+            this.lblOctal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOctal.Location = new System.Drawing.Point(476, 219);
+            this.lblOctal.Margin = new System.Windows.Forms.Padding(0);
+            this.lblOctal.Name = "lblOctal";
+            this.lblOctal.Size = new System.Drawing.Size(69, 29);
+            this.lblOctal.TabIndex = 20;
+            this.lblOctal.Text = "Octal";
+            // 
+            // lblHexadecimal
+            // 
+            this.lblHexadecimal.AutoSize = true;
+            this.lblHexadecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHexadecimal.Location = new System.Drawing.Point(309, 219);
+            this.lblHexadecimal.Margin = new System.Windows.Forms.Padding(0);
+            this.lblHexadecimal.Name = "lblHexadecimal";
+            this.lblHexadecimal.Size = new System.Drawing.Size(153, 29);
+            this.lblHexadecimal.TabIndex = 19;
+            this.lblHexadecimal.Text = "Hexadecimal";
+            // 
+            // lblDecimal
+            // 
+            this.lblDecimal.AutoSize = true;
+            this.lblDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDecimal.Location = new System.Drawing.Point(196, 219);
+            this.lblDecimal.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDecimal.Name = "lblDecimal";
+            this.lblDecimal.Size = new System.Drawing.Size(101, 29);
+            this.lblDecimal.TabIndex = 18;
+            this.lblDecimal.Text = "Decimal";
+            // 
+            // lblBinary
+            // 
+            this.lblBinary.AutoSize = true;
+            this.lblBinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinary.Location = new System.Drawing.Point(82, 219);
+            this.lblBinary.Margin = new System.Windows.Forms.Padding(0);
+            this.lblBinary.Name = "lblBinary";
+            this.lblBinary.Size = new System.Drawing.Size(80, 29);
+            this.lblBinary.TabIndex = 17;
+            this.lblBinary.Text = "Binary";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(323, 120);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(0);
+            this.txtInput.MaxLength = 19;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(227, 32);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(72, 120);
+            this.lblInput.Margin = new System.Windows.Forms.Padding(0);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(183, 29);
+            this.lblInput.TabIndex = 15;
+            this.lblInput.Text = "Enter a number:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(155, 28);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(309, 37);
+            this.lblTitle.TabIndex = 14;
+            this.lblTitle.Text = "Conversion System";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 490);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.txtOctal);
+            this.Controls.Add(this.txtHexadecimal);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.txtBinary);
+            this.Controls.Add(this.lblOctal);
+            this.Controls.Add(this.lblHexadecimal);
+            this.Controls.Add(this.lblDecimal);
+            this.Controls.Add(this.lblBinary);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.TextBox txtOctal;
+        private System.Windows.Forms.TextBox txtHexadecimal;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.TextBox txtBinary;
+        private System.Windows.Forms.Label lblOctal;
+        private System.Windows.Forms.Label lblHexadecimal;
+        private System.Windows.Forms.Label lblDecimal;
+        private System.Windows.Forms.Label lblBinary;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ Ass-1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ass_1
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Assignment 1 for team members:
+        /// Erica Teixeira - 200416441
+        /// Diya Shrestha - 200407713
+        /// Sasin C. Pradhan - 200409259
+        /// Dorukhan Colak - 200423172
+        /// Tram Mai -200425197
+        /// Zishanali Murji - 200424007
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConverter_Click(object sender, EventArgs e)
+        {
+            ConvertNumber();
+        }
+
+        private void ConvertNumber()
+        {
+            string input = txtInput.Text;
+            int nInput = int.Parse(input);
+
+            txtDecimal.Text = txtInput.Text;
+            convertToBinary(nInput);
+            convertToHexa(nInput);
+            convertToOctal(nInput);
+        }
+
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtBinary.Clear();
+            txtDecimal.Clear();
+            txtInput.Clear();
+            txtOctal.Clear();
+            txtHexadecimal.Clear();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void convertToBinary(int nInput)
+        {
+            txtBinary.Clear();
+            int i;
+            int[] binarryArr = new int[100];
+
+            for (i = 0; nInput > 0; i++)
+            {
+                binarryArr[i] = (nInput % 2);
+                nInput = nInput / 2;
+            }
+
+            for (i = i - 1; i >= 0; i--)
+            {
+                txtBinary.Text = txtBinary.Text + binarryArr[i];
+            }
+        }
+
+        private void convertToHexa(int nInput)
+        {
+            txtHexadecimal.Clear();
+            int i = 0;
+            decimal[] hexaArr = new decimal[100];
+            decimal remainder;
+
+            while (nInput != 0)
+            {
+                remainder = (nInput % 16);
+
+                if (remainder < 10)
+                    remainder += 48;
+                else
+                    remainder += 55;
+
+                hexaArr[i++] = remainder;
+                nInput = nInput / 16;
+            }
+
+            for (i = i - 1; i >= 0; i--)
+            {
+                txtHexadecimal.Text = txtHexadecimal.Text + (char)hexaArr[i];
+            }
+        }
+
+        private void convertToOctal(int nInput)
+        {
+            txtOctal.Clear();
+            int i;
+            int[] octalArr = new int[100];
+
+            for (i = 0; nInput > 0; i++)
+            {
+                octalArr[i] = (nInput % 8);
+                nInput = nInput / 8;
+            }
+
+            for (i = i - 1; i >= 0; i--)
+            {
+                txtOctal.Text = txtOctal.Text + octalArr[i];
+            }
+        }
+    }
+}
